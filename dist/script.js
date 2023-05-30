660b95e88dc0c951a386d8a45881d4972fe2561a
akash457353564 <102581699+akash457353564@users.noreply.github.com>
2023-05-30 18:10:06 +0530

    commit

--- dist/script.js
+++ dist/script.js
@@ -574,6 +574,33 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"6rimH":[function(require,module,exports) {
+const male_cards_wrapper = document.querySelector(".cards_wrapper");
+const get_profiles = async function(gender, religion, caste, cards_container) {
+    const res = await fetch(`https://api.betterhalf.ai/v2/search-users?limit=6&gender=${gender}&religions=${religion}&caste=${caste}`);
+    const data = await res.json();
+    const profiles = data.results;
+    console.log(profiles);
+    profiles.forEach(function(profile) {
+        cards_container.insertAdjacentHTML("afterbegin", `<div class="profile_block"><img src=${profile.images[0]} loading="lazy" alt="" class="profile_pic">
+        <div class="profile_txt">${profile.age}</div>
+        
+        <div class="profile_txt">${profile.caste}, ${profile.religion}</div>
+        
+        <div class="profile_txt">${profile.locations.features[0].properties.city}, ${profile.locations.features[0].properties.state}</div>
+        <div class="profile_txt">Last active time</div></div>`);
+        const last_seen = profile.active_at;
+        const last_seen_date = new Date(last_seen);
+        const last_seen_time = last_seen_date.toLocaleTimeString("en-IN", {
+            hour12: false
+        });
+        const now = new Date();
+        const current_time = now.toLocaleTimeString("en-IN", {
+            hour12: false
+        });
+    //console.log(current_time - last_seen_time)
+    });
+};
+get_profiles("male", "hindu", "aheer", male_cards_wrapper);
 
 },{}]},["gAoaA","6rimH"], "6rimH", "parcelRequire6676")
 
